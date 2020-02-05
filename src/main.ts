cbf5adc1a6e5efb5c071193be5e1504868763206
dqn <suyarizzz@gmail.com>
2020-02-06 00:38:32 +0900

    feat: load local video

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,45 @@
-console.log('hello');
+function $<E extends Element = Element>(selectors: string): E | null {
+  return document.querySelector<E>(selectors);
+}
+
+function isHTMLInputElement(value: any): value is HTMLInputElement {
+  return value?.files !== undefined;
+}
+
+function getFileFromEvent(event: Event): File | null {
+  const { target } = event;
+
+  if (!isHTMLInputElement(target)) {
+    return null;
+  }
+
+  const { files } = target;
+
+  if (!files || !files.length) {
+    return null;
+  }
+
+  return files.item(0);
+}
+
+function setup() {
+  const video = $<HTMLElement>('#video');
+  const file = $<HTMLElement>('#file');
+
+  if (!video || !file) {
+    return;
+  }
+
+  file.addEventListener('change', (event: Event) => {
+    const file = getFileFromEvent(event);
+
+    if (!file) {
+      return;
+    }
+
+    const src = URL.createObjectURL(file);
+    video.setAttribute('src', src);
+  });
+}
+
+setup();
